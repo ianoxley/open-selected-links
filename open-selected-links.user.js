1c7eb59773f67b16f3bc5344363cb308720e5a1d
ianoxley <ijoxley@gmail.com>
2010-06-03 23:21:16 +0100

    Added keydown event handler to detect ctrl / cmd key.
    Instead of just selecting the text, you now have to ctrl / cmd + select text in the same way that you ctrl / cmd + click to open a link in a new tab.

--- open-selected-links.user.js
+++ open-selected-links.user.js
@@ -5,25 +5,41 @@
 // @include        *
 // ==/UserScript==
 (function() {
-	document.addEventListener('mouseup', function() {
-		var selection = window.getSelection();
-		if (selection) {
-			var range = selection.getRangeAt(0);
-			var selectionDom = range.cloneContents();
-			var selectedLinks = selectionDom.querySelectorAll('a');
-			
-			var link;
-			for (var i = selectedLinks.length - 1; i >= 0; i--){
-				link = selectedLinks[i];
-				if (isGoogleResultsPage() && 
-						(isGoogleCachedLink(link) || isGoogleSimilarLink(link))) {
-					continue;
-				}
-				
-				GM_openInTab(selectedLinks[i].href);
-			};
-		}
+	
+	document.addEventListener('keydown', function(e) {
+                var ctrlKeyCode = 17;
+                var cmdKeyCode = 224;
+
+                if (e.keyCode == ctrlKeyCode || e.keyCode == cmdKeyCode) {
+	            document.addEventListener('mouseup', openSelectedLinks, true);
+                }
 	}, true);
+
+        document.addEventListener('keyup', function(e) {
+            // Remove the mousedown handler so it doesn't open links just
+            // by selecting text *without* using ctrl / cmd
+            document.removeEventListener('mouseup', openSelectedLinks, true);
+        }, true);
+
+        function openSelectedLinks(e) {
+	    var selection = window.getSelection();
+	    if (selection) {
+	        var range = selection.getRangeAt(0);
+	        var selectionDom = range.cloneContents();
+                var selectedLinks = selectionDom.querySelectorAll('a');
+			
+                var link;
+		for (var i = selectedLinks.length - 1; i >= 0; i--){
+		    link = selectedLinks[i];
+		    if (isGoogleResultsPage() && 
+		            (isGoogleCachedLink(link) || isGoogleSimilarLink(link))) {
+			continue;
+	            }
+				
+	            GM_openInTab(selectedLinks[i].href);
+	        }
+	    }
+        }
 	
 	function isGoogleResultsPage() {
 		return document.location.host.indexOf('.google.') !== -1;
